72fca061f1d17e21d73c904ecd25ef161b8eea6a
chanjunren <chanjunren@gmail.com>
2022-05-01 17:23:43 +0800

    Fixed country card display uneven size

--- components/home/country_card.tsx
+++ components/home/country_card.tsx
@@ -5,15 +5,15 @@ import InfoField from '../shared/info_field';
 
 export default function CountryCard(country: Country) {
   return (
-    <Link href={`/countries/${country.name.common}`} passHref>
-      <div className="flex flex-col shadow-lg rounded-md w-[200px] min-w-fit pb-20 cursor-pointer hover:scale-[1.02] duration-100 dark:bg-control-dark">
+    <Link href={`/countries/${country.name.official}`} passHref>
+      <div className="flex flex-col shadow-lg rounded-md w-[200px] pb-20 cursor-pointer hover:scale-[1.02] duration-100 dark:bg-control-dark">
           <Image
             className="rounded-md"
             src={country.flags.png ? country.flags.png : ''}
             alt={country.name.common + '_flag'}
             height={120}
             width={200}
-            objectFit="fill"
+            objectFit="cover"
           />
         <div className="p-5">
           <h1 className="font-bold mt-5 mb-5">{country.name.common}</h1>
