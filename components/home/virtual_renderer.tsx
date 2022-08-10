3090b7716af211ed9dfa96377a7c318197ee8d48
Jun Ren <chanjunren@gmail.com>
2022-08-10 22:37:39 +0800

    Fixed eslint erros

--- components/home/virtual_renderer.tsx
+++ components/home/virtual_renderer.tsx
@@ -2,7 +2,7 @@
 
 import CountryCard from './country_card';
 
-import { useEffect, useRef, useState } from 'react';
+// import { useEffect, useRef, useState } from 'react';
 import { Country } from '../../lib/utils/country_types';
 
 interface VirtualRenderProps {
@@ -11,10 +11,10 @@ interface VirtualRenderProps {
   countries: Country[];
 }
 
-interface ScrollProps {
-  start: number;
-  end: number;
-}
+// interface ScrollProps {
+//   start: number;
+//   end: number;
+// }
 
 export default function VirtualRenderer({
   searchFilter,
@@ -23,47 +23,47 @@ export default function VirtualRenderer({
 }: VirtualRenderProps) {
   // https://stackoverflow.com/questions/49058890/how-to-get-a-react-components-size-height-width-before-render
 
-  const [scrollPort, setScrollPort] = useState<ScrollProps>({
-    start: 0,
-    end: 0,
-  });
+  // const [scrollPort, setScrollPort] = useState<ScrollProps>({
+  //   start: 0,
+  //   end: 0,
+  // });
 
-  const viewportRef = useRef<HTMLDivElement>(null);
+  // const viewportRef = useRef<HTMLDivElement>(null);
 
-  const [viewportHeight, setViewportHeight] = useState<number>(0);
+  // const [viewportHeight, setViewportHeight] = useState<number>(0);
 
-  const scrollEventListener = (event: Event) => {
-    // console.log(window.scrollY);
-    // console.log(viewportRef.current?.clientWidth);
-  };
+  // const scrollEventListener = (event: Event) => {
+  //   // console.log(window.scrollY);
+  //   // console.log(viewportRef.current?.clientWidth);
+  // };
 
-  const resizeEventListener = () => {
-    adjustViewPortSize();
-  };
+  // const resizeEventListener = () => {
+  //   adjustViewPortSize();
+  // };
 
-  const adjustViewPortSize = () => {
-    let numberOfCardsPerRow = Math.min(Math.floor(
-      viewportRef.current?.clientWidth! / 200
-    ), 4);
-      console.log(numberOfCardsPerRow);
-    let height = Math.ceil(countries.length / numberOfCardsPerRow) * 475;
-    console.log(height);
-    setViewportHeight(height);
-  }
+  // const adjustViewPortSize = () => {
+  //   const numberOfCardsPerRow = Math.min(Math.floor(
+  //     viewportRef.current?.clientWidth! / 200
+  //   ), 4);
+  //     console.log(numberOfCardsPerRow);
+  //   const height = Math.ceil(countries.length / numberOfCardsPerRow) * 475;
+  //   console.log(height);
+  //   setViewportHeight(height);
+  // }
 
-  useEffect(() => {
-    addEventListener('scroll', scrollEventListener);
-    addEventListener('resize', resizeEventListener);
-    adjustViewPortSize();
-    return () => {
-      removeEventListener('scroll', scrollEventListener);
-      removeEventListener('resize', resizeEventListener);
-    };
-  });
+  // useEffect(() => {
+  //   addEventListener('scroll', scrollEventListener);
+  //   addEventListener('resize', resizeEventListener);
+  //   adjustViewPortSize();
+  //   return () => {
+  //     removeEventListener('scroll', scrollEventListener);
+  //     removeEventListener('resize', resizeEventListener);
+  //   };
+  // });
 
   return (
     <div
-      ref={viewportRef}
+      // ref={viewportRef}
       id="viewport"
       className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-x-10 gap-y-28 p-10 w-5/6 m-auto justify-center align-center"
       // style={{ height: viewportHeight }}
